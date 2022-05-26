9f9ddd7206c637565e434014bd71807c30da7b0d
Narai Risser <naraiguy@gmail.com>
2022-05-26 12:53:06 -0400

    Calm before the storm

--- Sizzle RB/Assets/Testing/WeaverBody/SkeletonController.cs
+++ Sizzle RB/Assets/Testing/WeaverBody/SkeletonController.cs
@@ -8,14 +8,92 @@ public class SkeletonController : MonoBehaviour
 {
     // The target we are going to track
     [SerializeField] Transform target;
+    private Vector3 heldTargetPos;
+    private Vector3 currentAim;
+    public float lerp;
+    private Vector3 origin;
+
     // A reference to the gecko's neck
     [SerializeField] Transform headBone;
 
+    [SerializeField] float speed;
+    [SerializeField] float maxTurnAngle;
+    [SerializeField] float offsetRightAngle;
+    [SerializeField] float offsetUpAngle;
+
+    [SerializeField] float minTargetDisToMove;
+
+    private void Start()
+    {
+        origin = target.position;
+
+        heldTargetPos = target.position;
+        currentAim = target.position;
+
+        lerp = 0;
+    }
+
     void LateUpdate()
     {
-        Vector3 towardObjectFromHead = target.position - headBone.position;
+        LerpTarget();
+        LookAtTarget();
+    }
 
-        headBone.rotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
+    private void LerpTarget()
+    {
+        // If target has moved in world space
+        if (Vector3.Distance(heldTargetPos, target.position) >= minTargetDisToMove)
+        {
+            UpdateAim();
+        }
+        else
+        {
+            if(lerp >= 0.9)
+            {
+                UpdateAim();
+            }
+        }
 
+        if(lerp <= 1)
+        {
+            currentAim = Vector3.Lerp(origin, heldTargetPos, lerp);
+
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            origin = heldTargetPos;
+        }
+        
+    }
+
+    private void UpdateAim()
+    {
+        float disFromOrigin = Vector3.Distance(heldTargetPos, origin);
+        float totalDis = Vector3.Distance(target.position, origin);
+
+        // Updates lerp to account for changed target 
+        lerp = disFromOrigin / totalDis;
+        heldTargetPos = target.position;
+    }
+
+
+    /// <summary>
+    /// Adjusts the head to look towards the target position 
+    /// </summary>
+    private void LookAtTarget()
+    {
+        Vector3 towardObjectFromHead = currentAim - headBone.position;
+
+        headBone.rotation = Quaternion.LookRotation(towardObjectFromHead, -transform.up);
+
+        // Adjusts for y-axis being forward and not z-axis
+        headBone.eulerAngles -= Vector3.right * 90;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(currentAim, 0.1f);
     }
 }
